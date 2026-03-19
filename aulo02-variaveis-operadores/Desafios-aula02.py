d7b986f0528da068224041b496fdd3433c5b25bd
gustavomelo <gugs42000@gmail.com>
2026-03-19 20:57:08 -0300

    aula 2 varíaveis e operadores

---
+++ aulo02-variaveis-operadores/Desafios-aula02.py
@@ -0,0 +1,16 @@
+# DESAFIO 1
+
+nome = input("Digite o seu nome: ")
+print(f"Bem vindo(a) {nome}!")
+
+#DESAFIO 2
+dia = input("Digite o dia em que nasceu: ")
+mês = input("Digite o mês em que nasceu: ")
+ano = input("Digite o ano em que nasceu: ")
+print(f"você nasceu em: {dia} de {mês} de {ano}")
+
+#DESAFIO 3
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro nùmero: "))
+res = num1 + num2
+print(f"O resultado da some desses números é: {res}")
---
+++ aulo02-variaveis-operadores/Exercicios-aula02.py
@@ -0,0 +1,5 @@
+raio = 5
+pi = 3.14159
+area = pi * raio ** 2
+
+print()
