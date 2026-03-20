57eb2d73667e763ccc874ff5277542d4595a616b
gustavomelo <gugs42000@gmail.com>
2026-03-19 21:28:07 -0300

    aula 2 varíaveis e operadores

--- aulo02-variaveis-operadores/Exercicios-aula02.py
+++ aulo02-variaveis-operadores/Exercicios-aula02.py
@@ -1,5 +1,35 @@
+# EXERCÍCIO 1
+
 raio = 5
 pi = 3.14159
 area = pi * raio ** 2
 
-print()
+print(f"A area do circulo é de: {area}")
+
+# EXERCÍCIO 2
+
+Fahr = int(input("Digite um valor em Fahrenheit: "))
+Cel = float((Fahr - 32) * 5 / 9)
+print(f"Isso é igual a {Cel} graus Celsius")
+
+# EXERCÍCIO 3
+
+
+
+# EXERCÍCIO 4
+
+
+
+# EXERCÍCIO 5
+
+
+
+# EXERCÍCIO 6
+
+
+
+# EXERCÍCIO 7
+
+
+
+# EXERCÍCIO 8
