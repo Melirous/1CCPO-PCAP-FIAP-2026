8892b39d214796885147825a9d8786297f65ff51
melirous <gugs42000@gmail.com>
2026-04-09 20:58:38 -0300

    aula 03

---
+++ aula02B/aula02B-Operadores.py
@@ -0,0 +1,42 @@
+num1 = 4
+num2 = 2
+
+print(type(num1), type(num2))
+
+operacao = num1 / num2
+print(operacao, type(operacao))
+
+# operadores de atribuição
+num = 15
+print()
+print(num)
+
+num = num + 2
+print(num)
+
+num*= 2
+print(num)
+
+print()
+print(6 != 6)
+
+idade = 20
+print(idade >= 20)
+
+logado = True
+print(logado, type(logado))
+
+maior_idade = idade >= 18
+print(maior_idade)
+
+
+#strings
+nome1 = "Marcos"
+nome2 = "marcos"
+
+print(nome1.upper() == nome2.upper())
+
+
+
+
+
